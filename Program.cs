7a78c2822f83517a3fa0bc73999a0cfb4e044f29
Anton Maslov <anton-maslov3@mail.ru>
2023-02-25 15:27:51 +0300

    положительные числа массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Найти сумму положительных элементов массива
+// int FindSumPositiveElementsIn(int[] array)
+
+int SummPositive (int [] array)
+{
+    int length = array.Length;
+    int result = 0;
+    for (int i = 0; i<length; i++)
+        {
+            if (array[i] > 0) result = result + array [i] ;
+        }
+    return result;
+}
